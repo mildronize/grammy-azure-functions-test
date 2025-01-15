a30adec074b2d6c4951c92e12f616ca1110087d0
Thada Wangthammang <thada.wth@gmail.com>
2025-01-16 00:04:14 +0700

    feat: add simple bot

--- func-v3/telegramBot/index.ts
+++ func-v3/telegramBot/index.ts
@@ -1,13 +1,20 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import { webhookCallback } from 'grammy';
+import { BotService } from '../src/services/bot.service';
+
+if (!process.env.BOT_TOKEN) {
+  throw new Error("BOT_TOKEN is not defined")
+}
+const botService = new BotService({
+  botToken: process.env.BOT_TOKEN,
+});
+botService.init();
 
 const telegramBot: AzureFunction = async function (
   context: Context,
   request: HttpRequest,
 ): Promise<void> {
-  context.res = {
-    status: 200,
-    body: 'Hello from telegramBot!',
-  };
+  await webhookCallback(botService.bot, "azure")(context as any, request);
 };
 
 export default telegramBot;
